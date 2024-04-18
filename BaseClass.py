6167e61892f8e358fdd2f1772d755a023b77ac60
CrasherChris <CrasherChris@github.com>
2024-04-18 22:15:28 +0800

    Basic Skills

--- BaseClass.py
+++ BaseClass.py
@@ -1,4 +1,5 @@
 from Const import GameEvent
+import random
 class creature(object):
     """
     The base class of all creature
@@ -47,7 +48,37 @@ class creature(object):
             return GameEvent.LOSE_MP_OVER
         else:
             self.MP-=value
-            return 0 
+            return 0
+
+    def create_skill_set(self):
+        return self.SkillSet(self)
+
+    class SkillSet(object):
+        def __init__(self, outer):
+            self.outer = outer
+            self.able_to_act = True
+
+        def heal(self):
+            if self.outer.HP < 10:
+                self.outer.add_HP(20)
+            else:
+                self.outer.add_HP(10)
+
+        def silence(self):
+            self.able_to_act = False
+
+        def reset(self):
+            self.able_to_act = True
+
+        def change_carddeck(self):
+            pass
+
+        def attack(self, damage=10):
+            return damage
+
+        def defense(self, damage=10):
+            self.outer.lose_HP(damage)
+
         
 class card(object):
     card_num=3
@@ -65,3 +96,34 @@ class poker(object):
 
 class pokerdeck(object):
     pass
+
+
+class Hero(creature):
+    def __init__(self):
+        super().__init__()
+        self.skill_set = self.create_skill_set()
+        self.shock_wave = self.skill_set.silence
+        self.change_carddeck = self.skill_set.change_carddeck
+
+    def attack(self):   # 被动属性
+        if self.HP < 10:
+            return self.skill_set.attack(20)
+        else:
+            return self.skill_set.attack()
+
+    def defence(self):  # 被动属性
+        c = random.randint(1, 10)
+        if c < 3:
+            self.skill_set.defense(5)
+        else:
+            self.skill_set.defense()
+
+
+class Monster(creature):
+    def __init__(self):
+        super().__init__()
+        self.skill_set = self.create_skill_set()
+        self.shock_wave = self.skill_set.silence
+        self.attack = self.skill_set.attack
+        self.defence = self.skill_set.defense
+        self.change_carddeck = self.skill_set.change_carddeck
