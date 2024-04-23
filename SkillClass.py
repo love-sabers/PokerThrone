76e97e44a4502e472d633a1bd101062c5e0e9783
love_saber <love_saber1983@outlook.com>
2024-04-23 15:48:18 +0800

    update skills

--- SkillClass.py
+++ SkillClass.py
@@ -1,62 +1,133 @@
-from Const import GameEvent
+import pygame
 import random
+import GUI
+from GUI import Button
+from Const import GameEvent
 
 class Skill(object):
-    def __init__(self) -> None:
-        pass
-    pass
+    IMAGE_PATH='source/play.png'
+    def __init__(self,pos:tuple[int,int],handtype_check_code_list:int):
+        self.result = {'MP_cost': GameEvent.NULL,#int
+                       'HP_cost': GameEvent.NULL,#int
+                       'MP_increase': GameEvent.NULL,#int
+                       'HP_increase': GameEvent.NULL,#int
+                       'pos_effect': GameEvent.NULL,#list        # 正面效果是带给自己的正面效果
+                       'neg_effect': GameEvent.NULL,#list        # 负面效果是带给别人的负面效果
+                       'damage': GameEvent.NULL,#int
+                       'operate': GameEvent.NULL#list            # 对游戏其他部分的操作请求，如换牌
+        }
+        self.ui=Button(pos,self.IMAGE_PATH,3,GUI.CENTER)         
+        self.handtype_check_code_list=handtype_check_code_list
 
-class Shield(Skill):
-    def __init__(self, entity):
-        self.MP_cost = 3
-        self.entity = entity
+    def update(self,handtype_code:list[int]):
+        if any(self.handtype_check_code_list==code for code in handtype_code) :
+            self.ui.enabled()
+        else :
+            self.ui.disabled()
 
-    @property
-    def activate(self):
-        if self.MP_cost > self.entity.MP:       # 技能释放失败
-            return GameEvent.SKILL_RELEASE_FAIL
-        return GameEvent.IMMUNE
+    def render(self,surface:pygame.Surface):
+        self.ui.render(surface)
 
-class Medicine(Skill):
-    def __init__(self, entity):
-        self.MP_cost = 2
-        self.entity = entity
+    def check_click(self,event):
+        return self.ui.check_click(event)        
 
-    @property
-    def activate(self):
-        if self.MP_cost > self.entity.MP:  # 技能释放失败
-            return GameEvent.SKILL_RELEASE_FAIL
-        return GameEvent.HEAL
-
-class Rage(Skill):
-    def __init__(self, entity):
-        self.MP_cost = 3
-        self.entity = entity
-
-    @property
-    def activate(self):
-        if self.MP_cost > self.entity.MP:  # 技能释放失败
-            return GameEvent.SKILL_RELEASE_FAIL
-        return GameEvent.RAGE
-
-class Shockwave(Skill):
-    def __init__(self, entity):
-        self.MP_cost = 4
-        self.entity = entity
+class Shield(Skill):        # 普通技能
+    IMAGE_PATH=''
+    def __init__(self, pos: tuple[int, int], handtype_check_code_list: list[int]):
+        super().__init__(pos, handtype_check_code_list)
 
     @property
     def activate(self):
-        if self.MP_cost > self.entity.MP:   # 技能释放失败
-            return GameEvent.SKILL_RELEASE_FAIL
-        return GameEvent.ATTACK, GameEvent.SHOCK
+        self.result['pos_effect'] = [GameEvent.IMMUNE]
+        return self.result
 
-class Reshuffle(Skill):
-    def __init__(self, entity):
-        self.MP_cost = 1
-        self.entity = entity
+
+class Medicine(Skill):          # 普通技能
+    def __init__(self, pos: tuple[int, int], handtype_check_code_list: list[int]):
+        super().__init__(pos, handtype_check_code_list)
+        self.result['HP_increase'] = 5
+    @property
+    def activate(self):
+        return self.result
+
+
+class Reshuffle(Skill):         # 普通技能
+    def __init__(self, pos: tuple[int, int], handtype_check_code_list: list[int]):
+        super().__init__(pos, handtype_check_code_list)
 
     @property
     def activate(self):
-        if self.MP_cost > self.entity.MP:
-            return GameEvent.SKILL_RELEASE_FAIL
-        return Reshuffle
+        self.result['operate'] = [GameEvent.RESHUFFLE]
+        return self.result
+
+
+class Attack(Skill):
+    def __init__(self, pos: tuple[int, int], handtype_check_code_list: list[int]):
+        super().__init__(pos, handtype_check_code_list)
+        self.result['damage'] = 10
+    @property
+    def activate(self):
+        return self.result
+
+
+class Rage(Skill):              # 较强技能
+    def __init__(self, pos: tuple[int, int], handtype_check_code_list: list[int]):
+        super().__init__(pos, handtype_check_code_list)
+    @property
+    def activate(self):
+        self.result['HP_cost'] = 5
+        self.result['pos_effect'] = [GameEvent.RAGE]
+        return self.result
+
+
+class Shockwave(Skill):         # 较强技能
+    def __init__(self, pos: tuple[int, int], handtype_check_code_list: list[int]):
+        super().__init__(pos, handtype_check_code_list)
+    @property
+    def activate(self):
+        self.result['neg_effect'] = [GameEvent.SHOCK]
+        self.result['damage'] = 10
+        return self.result
+
+
+class TrickBag(Skill):      # 较强技能
+    def __init__(self, pos: tuple[int, int], handtype_check_code_list: list[int]):
+        super().__init__(pos, handtype_check_code_list)
+    @property
+    def activate(self):
+        dice = random.randint(1, 6)     # 投掷骰子，随机抽取下面三个能力中的一个
+        if dice <= 2:
+            self.result['HP_cost'] = 5        # 扣5点生命并造成20伤害
+            self.result['damage'] = 20
+        elif dice <= 4:
+            self.result['HP_increase'] = 10     # 恢复10点生命
+        else:
+            self.result['operate'] = [GameEvent.RESHUFFLE]    # 重新抽牌并获得一个盾
+            self.result['pos_effect'] = [GameEvent.IMMUNE]
+        return self.result
+
+
+class Poison(Skill):
+    def __init__(self, pos: tuple[int, int], handtype_check_code_list: list[int]):
+        super().__init__(pos, handtype_check_code_list)
+
+    @property
+    def activate(self):
+        dice = random.randint(1, 6)  # 投掷骰子，决定炸弹炸不炸
+        if dice <= 3:
+            self.result['damage'] = 20      # 毒药有效，否则哑弹
+            self.result['neg_effect'] = [GameEvent.POISONED]
+        else:
+            self.result['damage'] = 5
+        return self.result
+
+
+class Ultimate(Skill):       # 牛逼的大招
+    def __init__(self, pos: tuple[int, int], handtype_check_code_list: list[int]):
+        super().__init__(pos, handtype_check_code_list)
+    @property
+    def activate(self):
+        self.result['damage'] = 20
+        self.result['neg_effect'] = [GameEvent.SHOCK]
+        self.result['pos_effect'] = [GameEvent.RAGE, GameEvent.PURIFY, GameEvent.IMMUNE]
+        return self.result
